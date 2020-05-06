077a1807a78c5d1814dcbb885c8f9a5324e3b1e0
Muralidhar <muralidharreddy80@gmail.com>
2020-05-06 12:18:05 +0530

    state changes in app.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,43 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+   state = {
+     person : [
+       {name:'Murali', age:25},
+       {name:'Uday', age:20},
+       {name:'Sushma', age: 22},
+       {name:'Vishnu', age: 30}
+     ],
+     otherState: 'some other state value'
+   }
+
+   switchNameHandler = () => {
+     console.log("i was in onclick button ");
+
+     this.setState({
+       person: [
+        {name:'Muralidhar Reddy', age:25},
+        {name:'Uday', age:20},
+        {name:'Sushma SangiReddy', age: 22},
+        {name:'Vishnu Vardhan', age: 30}
+      ]
+     })
+   }
+
   render(){
     return (
       <div className="App">
          
            <h2>i am working as React.js developer</h2> 
             Learn React
-           <Person name="Murali" age="25" />
-           <Person name="Uday" age="20" />
-           <Person name="Sushma" age="24"> MY JOB: SOFTWARE ENGINEER</Person>
-           <Person name="Swathi" age="23"/>
+
+            <br></br>
+            <button onClick={this.switchNameHandler}>Switch Name</button>
+           <Person name={this.state.person[0].name} age={this.state.person[0].age} />
+           <Person name={this.state.person[1].name} age={this.state.person[1].age} />
+           <Person name={this.state.person[2].name} age={this.state.person[2].age}> MY JOB: SOFTWARE ENGINEER</Person>
+           <Person name={this.state.person[3].name} age={this.state.person[3].age}/>
         
       </div>
     );
